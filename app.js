5625e0ae4691a50a147df2fb51ee66dfac810ed4
iamshiv007 <Github@#007>
2023-11-04 20:50:35 +0530

    some code committed

--- app.js
+++ app.js
@@ -39,13 +39,13 @@ app.get("/", (req, res) => {
     res.send("Test success")
 })
 
-// Find 404 and hand over to error handler
-app.use((req, res, next) => {
-    next(createError(404))
-})
-// error handler
-app.use(function (err, req, res, next) {
-    console.log(err.message) // Log error message in our server's console
-    if (!err.statusCode) err.statusCode = 500 // If err has no specified error code, set error code to 'Internal Server Error (500)'
-    res.status(err.statusCode).send(err.message) // All HTTP requests must have a response, so let's send back an error with its status code and message
-})
+// // Find 404 and hand over to error handler
+// app.use((req, res, next) => {
+//     next(createError(404))
+// })
+// // error handler
+// app.use(function (err, req, res, next) {
+//     console.log(err.message) // Log error message in our server's console
+//     if (!err.statusCode) err.statusCode = 500 // If err has no specified error code, set error code to 'Internal Server Error (500)'
+//     res.status(err.statusCode).send(err.message) // All HTTP requests must have a response, so let's send back an error with its status code and message
+// })
